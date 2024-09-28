ebdb261bb289e7086db865f5b54ba6b47228fc94
YARzich <yaric002@gmail.com>
2024-09-28 19:26:29 +0300

    changing the color space on the Lab

--- lazer.py
+++ lazer.py
@@ -9,49 +9,54 @@ def nothing(x):
 class Lazer:
 
     def __init__(self):
-        cv2.namedWindow('lazer')
-
-        cv2.createTrackbar('HL', 'lazer', 36, 180, nothing)
-        cv2.createTrackbar('SL', 'lazer', 29, 255, nothing)
-        cv2.createTrackbar('VL', 'lazer', 199, 255, nothing)
-        cv2.createTrackbar('H', 'lazer', 98, 180, nothing)
-        cv2.createTrackbar('S', 'lazer', 255, 255, nothing)
-        cv2.createTrackbar('V', 'lazer', 255, 255, nothing)
+        cv2.namedWindow('lazer_Lab')
+        cv2.createTrackbar('L_min', 'lazer_Lab', 218, 255, nothing)
+        cv2.createTrackbar('A_min', 'lazer_Lab', 70, 255, nothing)
+        cv2.createTrackbar('B_min', 'lazer_Lab', 100, 255, nothing)
+        cv2.createTrackbar('L_max', 'lazer_Lab', 255, 255, nothing)
+        cv2.createTrackbar('A_max', 'lazer_Lab', 120, 255, nothing)
+        cv2.createTrackbar('B_max', 'lazer_Lab', 138, 255, nothing)
 
         self.kernel = np.ones((2, 2), np.uint8)
 
     def lazer_detected(self, img):
-        hl = cv2.getTrackbarPos('HL', 'lazer')
-        sl = cv2.getTrackbarPos('SL', 'lazer')
-        vl = cv2.getTrackbarPos('VL', 'lazer')
 
-        hh = cv2.getTrackbarPos('H', 'lazer')
-        hs = cv2.getTrackbarPos('S', 'lazer')
-        hv = cv2.getTrackbarPos('V', 'lazer')
+        # result_mask_lazer = self.create_mask_HSV(img)
+        result_mask_lazer = self.create_mask_Lab(img)
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        lower_lazer = (hl, sl, vl)
-        upper_lazer = (hh, hs, hv)
-
-        mask_lazer = cv2.inRange(hsv, lower_lazer, upper_lazer)
-
-        result_mask_lazer = cv2.morphologyEx(cv2.morphologyEx(mask_lazer, cv2.MORPH_OPEN, self.kernel),
-                                             cv2.MORPH_CLOSE, self.kernel)
-
-        # result_mask_lazer_cpu = result_mask_lazer.download()qw
-
-        contours_lazer, _ = cv2.findContours(result_mask_lazer, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_lazer = self.contours(result_mask_lazer)
 
         x, y, w, h = 0, 0, 0, 0
 
         if contours_lazer:
-            area = cv2.contourArea(contours_lazer[0])
-            # if area > 1:
             x, y, w, h = cv2.boundingRect(contours_lazer[0])
-            # cv2.drawContours(img, [contours_lazer[0]], -1, (0, 255, 0), 2)
-            # cv2.putText(img, "KILL ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        cv2.imshow('lazer', result_mask_lazer)
 
         return x, y
+
+    def contours(self, mask_lazer):
+        contours_lazer, _ = cv2.findContours(mask_lazer, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        return contours_lazer
+
+    def create_mask_Lab(self, img):
+        l_min = cv2.getTrackbarPos('L_min', 'lazer_Lab')
+        a_min = cv2.getTrackbarPos('A_min', 'lazer_Lab')
+        b_min = cv2.getTrackbarPos('B_min', 'lazer_Lab')
+
+        l_max = cv2.getTrackbarPos('L_max', 'lazer_Lab')
+        a_max = cv2.getTrackbarPos('A_max', 'lazer_Lab')
+        b_max = cv2.getTrackbarPos('B_max', 'lazer_Lab')
+
+        lab = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+
+        lower_lazer = (l_min, a_min, b_min)
+        upper_lazer = (l_max, a_max, b_max)
+
+        mask_lazer = cv2.inRange(lab, lower_lazer, upper_lazer)
+
+        result_mask_lazer = cv2.morphologyEx(cv2.morphologyEx(mask_lazer, cv2.MORPH_OPEN, self.kernel),
+                                             cv2.MORPH_CLOSE, self.kernel)
+
+        cv2.imshow('lazer_Lab', result_mask_lazer)
+
+        return result_mask_lazer
